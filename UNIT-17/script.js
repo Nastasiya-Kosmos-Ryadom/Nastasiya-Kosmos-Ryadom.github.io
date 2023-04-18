0a51fa87a11d20a3e098171ed8a0de4009473deb
Anastasiya Mitrofanova <Anastasiya-mitrofanova@mail.ru>
2023-04-18 23:30:10 +0500

    домашка 17 не доделана

--- UNIT-17/script.js
+++ UNIT-17/script.js
@@ -1,1 +1,11 @@
 alert('подключили js из отдельного файла')
+var ColorPrimary = 'blue'
+let ColorSecond = 'red'
+const ColorColor ='green'
+let SomeColor
+SomeColor ='aqua'
+
+console.log(ColorSecond)
+console.log(ColorColor)
+console.log(ColorPrimary)
+console.log(SomeColor)
