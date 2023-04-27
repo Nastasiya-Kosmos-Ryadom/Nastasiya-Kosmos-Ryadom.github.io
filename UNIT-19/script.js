0a83f239287e101216b79a4e91c67ccea23b621d
Anastasiya Mitrofanova <Anastasiya-mitrofanova@mail.ru>
2023-04-28 01:10:49 +0500

    домашка 19 готовая вопросы в комментах

--- UNIT-19/script.js
+++ UNIT-19/script.js
@@ -26,6 +26,37 @@ let indexValue1 = number.unshift(77);
 console.log(indexValue1);/*возвращается индекс добавленного элемента */
 console.log(number);
 
+/*удаление и добавление конкретных эллементов массива */
 number.splice(1, 2, 55);/* (индекс с которого начинается действие, сколько элементов удаляем(можно ставить 0, тогда не удалится ничего и просто добавятся элементы), что ставим на их место(можно ничего не добавлять)) */
 console.log(number);
 
+/*Объединение массивов */
+console.log(number.concat(['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье']));
+
+/*Объединение массивов */
+let newArre = [11, 22, 33, 44, 55, 66, 77];
+console.log(number.concat(newArre));
+
+/*Получить подмассив (при этом последний индекс не будет выведен)*/
+console.log(newArre.slice(0,5))
+
+/*возведение в квадрат каждого элемента массива */
+newArre.forEach(function(item) {
+    console.log('item*item', item*item)
+})
+
+/*перевернуть массив */
+console.log(newArre.reverse());
+
+/*возврат результирующей функции */
+let Day = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
+let firstDay = Day.map(function(item, index, array){
+    return 'first ' + item;
+})
+console.log(firstDay);
+
+/*метод фильтр */
+let filter = Day.filter(function (item, index, array){
+    return item !== 'воскресенье';
+});
+console.log(filter);
